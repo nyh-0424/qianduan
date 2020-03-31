861b49f27128b7638fb43ea3dd5cd59c692083f4
nyh-0424 <2898632166@qq.com>
2020-03-31 18:32:44 +0800

    作业3 23-nanyihan finish time stamp

--- post_youdao.py
+++ post_youdao.py
@@ -1,15 +1,33 @@
 import requests
 
 url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+
+
+def get_salt():
+    return '15846844357889'
+
+
+def get_sign():
+    return '4cf44da69384da8fb5c2364a31b22380'
+
+
+def get_ts():
+    import  time
+    t = time.time()
+    ts = str(int(round(t * 1000)))
+    return ts    #'1584684435788'
+          # '1585615400595'
+
+
 form_data={
     'i':'我和你',
     'from': 'AUTO',
     'to': 'AUTO',
     'smartresult': 'dict',
     'client': 'fanyideskweb',
-    'salt': '15846844357889',
-    'sign': '4cf44da69384da8fb5c2364a31b22380',
-    'ts': '1584684435788',
+    'salt': get_salt(),
+    'sign': get_sign(),
+    'ts': get_ts(),
     'bv': '70244e0061db49a9ee62d341c5fed82a',
     'doctype': 'json',
     'version': '2.1',
