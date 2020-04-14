45f6e114a703d270c70e3f675ff90570d3499701
nyh-0424 <2898632166@qq.com>
2020-04-14 09:54:57 +0800

    作业6 23-nanyihan finished post data

--- post_youdao.py
+++ post_youdao.py
@@ -1,55 +1,42 @@
 import random
 import  time
 import requests
+import json
+import hashlib
 
-# url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
-# content='我和你'
 
 
 class  Youdao():
     def __init__(self,content):
         self.content=content
-        self.url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+        self.url='http://fanyi.youdao.com/translate'
         self.ts=self.get_ts()
         self.salt=self.get_salt()
         self.sign=self.get_sign()
 
     def get_salt(self):
-      s=str(random.randint(0,10))
-      t=self.ts
-      # print("random=",s)
-      # print("ts=",t)
-      # print("salt=",t+s)
-      return t+s
-     # return '15846844357889'
+     return self.ts+str(random.randint(0,10))
+
 
 
     def get_md5(self,value):
-      import hashlib
       m = hashlib.md5()
       m.update(value.encode("utf-8"))
       return m.hexdigest()
 
 
     def get_sign(self):
-      i=self.salt
-      e=self.content
-      s="fanyideskweb" + e + i + "Nw(nmmbP%A-r6U3EUn]Aj"
-    # print("s=",s,"  md5=",get_md5(s))
+      s="fanyideskweb" + self.content + self.salt + "Nw(nmmbP%A-r6U3EUn]Aj"
       return self.get_md5(s)
-    # return '4cf44da69384da8fb5c2364a31b22380'
+
 
 
     def get_ts(self):
       t = time.time()
-      ts = str(int(round(t * 1000)))
-      print("ts=",ts)
-      return ts    #'1584684435788'
+      return str(int(round(t * 1000)))
 
-    # def get_content(self):
-    #  return content
 
-    def yield_from_data(self):
+    def yield_form_data(self):
        form_data={
           'i': self.content,
           'from': 'AUTO',
@@ -77,22 +64,17 @@ class  Youdao():
       return headers
 
     def fanyi(self):
-      import json
       response = requests.post(self.url,data=self.yield_form_data(),headers=self.get_headers())
       content=json.loads(response.text)
-      return content['translateResult'][0][0]['tggt']
+      return content['translateResult'][0][0]['tgt']
+
 
 
 
 
 if __name__ == '__main__':
-    # ts=get_ts()
-    # print(form_data)
-    # print(get_headers())
-    youdao=Youdao()
-    print(youdao.fanyi())
-    while(true):
-        content=input("please input : ")
-        print(Youdao(content).fanyi())
-    # response=requests.post(url,data=form_data,headers=get_headers())
-    # print(response.text)
+
+    while(True):
+        i=input("please input : ")
+        youdao=Youdao(i)
+        print("fanyi result :",youdao.fanyi())
